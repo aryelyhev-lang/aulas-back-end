b79c430ee898271d091370985da2b72bdd331108
aryely-lang <25203777@CFP127ALUNO.LOCAL>
2026-02-04 11:28:15 -0300

    iniciando atividade juros compostos

---
+++ atividade01-backend/app.js
@@ -0,0 +1,13 @@
+/**************************************************************************************
+ * Objetivo: Calcular os juros compostos
+ * Data: 04/02/2026
+ * Autor: Aryey Hevylyn
+ * Versão: 1.0
+ * ************************************************************************************/
+
+const readline = require("readline")
+
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}) 
